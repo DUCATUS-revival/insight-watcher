3cf6d73bf366ec1ec19592ab78a27a457b3eea33
Noah Hunter <nhhntr11@gmail.com>
2018-01-05 23:20:01 +0000

    Minor changes

--- index.js
+++ index.js
@@ -7,115 +7,134 @@ const request = require('request')
 
 const EventEmitter = require('events').EventEmitter;
 
-module.exports = function(args) { 
-  this.insight_servers = args.insight_servers;
-  this.insight_apis_servers = args.insight_api_endpoints;
-  this.connected = false
-  self = this
-  this.events = new EventEmitter()
-  this.getTxs = function(address) {
-    return new Promise(function(Success, Reject) {
-      self.getAddress(address).then(function(result) {
-        Success(result.txs)
-      })
-    })
-  }
-  this.getBalance = function(address) {
-    return new Promise(function(Success, Reject) {
-      self.getAddress(address).then(function(result) {
-        Success(result.balance)
-      }).catch(Reject)
-    })
-  }
-  this.getAddress = function(address) {
-    var result = {}
-    blockdebug('Getting txs for address', address, 'url:', self.api_url + 'txs/?address=' + address)
-    return new Promise(function(Success, Reject) {
-      result.address = address
-      result.in = 0
-      result.out = 0
-      result.curr = `bits(u%{args.crypto_symbol})`
-      request(self.api_url + 'txs/?address=' + address, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-          blockdebug('success :)')
+function InsightWatcher(args) { 
+	this.insight_servers = [];
+	this.insight_apis_servers = [];
+	this.crypto_symbol = "";
+	this.connected = false
+	self = this
+	this.events = new EventEmitter()
+	this.getTxs = function(address) {
+		return new Promise(function(Success, Reject) {
+			self.getAddress(address).then(function(result) {
+				Success(result.txs)
+			})
+		})
+	}
+	this.getData = function(address) {
+		return new Promise(function(Success, Reject) {
+			self.getAddress(address).then(function(result) {
+				Success(result)
+			})
+		})
+	}
+	this.getBalance = function(address) {
+		return new Promise(function(Success, Reject) {
+			self.getAddress(address).then(function(result) {
+				Success(result.balance)
+			}).catch(Reject)
+		})
+	}
+	this.init = function(args) {
+		self.insight_servers = args.insight_servers;
+		self.insight_apis_servers = args.insight_api_endpoints;
+		self.crypto_symbol = args.crypto_symbol;
+	}
+	this.getAddress = function(address) {
+		var result = {}
+		blockdebug('Getting txs for address', address, 'url:', self.api_url + 'txs/?address=' + address)
+		return new Promise(function(Success, Reject) {
+			result.address = address
+			result.in = 0
+			result.out = 0
+			result.curr = `bits(u${self.crypto_symbol})`
+			console.log(self.api_url + 'txs/?address=' + address);
+			request(self.api_url + 'txs/?address=' + address, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					blockdebug('success :)')
 
-          var transaction_json = JSON.parse(body)
-            // console.log('transaction_json')
-          transaction_json.txs.forEach(function(each_tx) {
-            each_tx.vout.forEach(function(each_vout) {
-              each_vout.scriptPubKey.addresses.forEach(function(outaddress) {
-                // console.log('checking', outaddress)
-                if (outaddress === address) {
-                  // console.log('adding!', each_vout.value)
-                  result.in = result.in + each_vout.value * 1000000
-                }
-              })
-            })
-            each_tx.vin.forEach(function(each_vin) {
+					var transaction_json = JSON.parse(body)
+					// console.log('transaction_json')
+					transaction_json.txs.forEach(function(each_tx) {
+						each_tx.vout.forEach(function(each_vout) {
+							each_vout.scriptPubKey.addresses.forEach(function(outaddress) {
+								// console.log('checking', outaddress)
+								if (outaddress === address) {
+									// console.log('adding!', each_vout.value)
+									result.in = result.in + each_vout.value * 1000000
+								}
+							})
+						})
+						each_tx.vin.forEach(function(each_vin) {
 
-              // each_vin.scriptPubKey.addresses.forEach(function(outaddress) {
-              // console.log('checking', outaddress)
-              if (each_vin.addr === address) {
-                // console.log('adding!', each_vout.value)
-                result.out = result.out + each_vin.value * 1000000
-              }
-              // })
+							// each_vin.scriptPubKey.addresses.forEach(function(outaddress) {
+							// console.log('checking', outaddress)
+							if (each_vin.addr === address) {
+								// console.log('adding!', each_vout.value)
+								result.out = result.out + each_vin.value * 1000000
+							}
+							// })
 
-            })
-          })
-          result.balance = result.in - result.out
-          result.txs = transaction_json.txs.length
-          Success({ txs: JSON.parse(body), balance: result })
-            // Success({ result: result })
-        } else {
-          blockdebug('reject')
-          Reject(response.statusCode)
-        }
-      })
-    })
-  }
-  this.connect = function() {
-    return new Promise(function(Success, Reject) {
-      if (self.connected === false) {
-        self.url = self.insight_servers.shift()
-        self.api_url = self.insight_apis_servers.shift()
-        if (self.url != undefined) {
-          self.socket = io(self.url);
-          setTimeout(function() {
-            if (self.connected === false) {
-              debug('Could not connect, trying again...')
-              self.socket.disconnect()
-              self.connect()
-            }
-          }, 5000)
+						})
+					})
+					result.balance = result.in - result.out
+					result.txs = transaction_json.txs.length
+					Success({ txs: JSON.parse(body), balance: result })
+					// Success({ result: result })
+				} else {
+					blockdebug('reject')
+					Reject(response.statusCode)
+				}
+			})
+		})
+	}
+	this.connect = function() {
+		return new Promise(function(Success, Reject) {
+			if (self.connected === false) {
+				self.url = self.insight_servers.shift()
+				self.api_url = self.insight_apis_servers.shift()
+				if (self.url != undefined) {
+					self.socket = io(self.url);
+					setTimeout(function() {
+						if (self.connected === false) {
+							debug('Could not connect, trying again...')
+							self.socket.disconnect()
+							self.connect()
+						}
+					}, 5000)
 
-          self.socket.on('connect', function() {
-            Success()
-            self.connected = true
-            self.socket.emit('subscribe', 'inv')
-            self.events.emit('connected')
-          });
-          self.socket.on('tx', function(data) {
-            self.events.emit('tx', data)
-            data.vout.forEach(function(each_vout) {
-              hashdebug({ address: Object.keys(each_vout)[0], amount: each_vout[Object.keys(each_vout)[0]] })
-              self.events.emit(Object.keys(each_vout)[0], { address: Object.keys(each_vout)[0], amount: each_vout[Object.keys(each_vout)[0]] });
-            })
-            txdebug("New transaction received: " + JSON.stringify(data))
-          })
-          self.socket.on('event', function(data) {
-            debug('event', data)
-          });
-          self.socket.on('disconnect', function(d) {
-            debug('disconnect!', d)
-          });
-          self.socket.on('error', function(e) {
-            debug('error!', e)
-          });
-        } else {
-          Reject('Cannot reach any bitcoin insight server... no bitcoin transactions are being received.')
-        }
-      }
-    });
-  }
+					self.socket.on('connect', function() {
+						Success()
+						self.connected = true
+						self.socket.emit('subscribe', 'inv')
+						self.events.emit('connected')
+					});
+					self.socket.on('tx', function(data) {
+						self.events.emit('tx', data)
+						data.vout.forEach(function(each_vout) {
+							hashdebug({ address: Object.keys(each_vout)[0], amount: each_vout[Object.keys(each_vout)[0]] })
+							self.events.emit(Object.keys(each_vout)[0], { data: data, address: Object.keys(each_vout)[0], amount: each_vout[Object.keys(each_vout)[0]] });
+						})
+						txdebug("New transaction received: " + JSON.stringify(data))
+					})
+					self.socket.on('event', function(data) {
+						debug('event', data)
+					});
+					self.socket.on('disconnect', function(d) {
+						debug('disconnect!', d)
+					});
+					self.socket.on('error', function(e) {
+						debug('error!', e)
+					});
+				} else {
+					Reject('Cannot reach any bitcoin insight server... no bitcoin transactions are being received.')
+				}
+			}
+		});
+	}
+	return this;
 }
+
+module.exports = function(args) {
+	return InsightWatcher(args);
+};
